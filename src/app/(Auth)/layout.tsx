89382105a6c0b1d61b713dcc9238b554decb1d3d
ethan <blazer030@gmail.com>
2024-03-27 21:58:32 +0800

    basic auth layout

--- src/app/(Auth)/layout.tsx
+++ src/app/(Auth)/layout.tsx
@@ -1,15 +1,85 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import { Button, Layout, Menu, theme } from "antd";
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined
+} from "@ant-design/icons";
 
 interface AuthLayoutProps {
     children: React.ReactNode;
 }
 
+const { Header, Sider, Content, Footer } = Layout;
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
     return (
-        <div>
-            Im AuthLayout
-            {children}
-        </div>
+        <Layout hasSider>
+            <Sider trigger={null}
+                   collapsible
+                   collapsed={collapsed}
+                   className="overflow-auto h-[100vh]"
+            >
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, background: colorBgContainer }}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content style={{ margin: '24px 16px 0', overflow: 'auto', height: 'calc(100vh - 88px)' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            textAlign: 'center',
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        {children}
+                    </div>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                    </Footer>
+                </Content>
+            </Layout>
+        </Layout>
     );
 }
 
